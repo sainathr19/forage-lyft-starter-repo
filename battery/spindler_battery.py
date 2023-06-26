4514c2fb09e202021ff3af02b870963c9a84c8f6
Sainath Reddy <88471927+sainathr19@users.noreply.github.com>
2023-06-26 22:19:58 +0530

    Refactoring Done

---
+++ battery/spindler_battery.py
@@ -0,0 +1,17 @@
+from battery import Battery
+from datetime import datetime
+
+
+class Splinder(Battery):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
+
+    def battery_needs_service(self):
+        service_threshold_date = self.last_service_date.replace(
+            year=self.last_service_date.year + 3)
+
+        if service_threshold_date < datetime.today().date():
+            return True
+        else:
+            return False
